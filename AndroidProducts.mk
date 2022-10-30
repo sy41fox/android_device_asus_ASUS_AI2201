d5c3a6a8b5186a8e6ac3fab852919fba86f2785f
Jean Rivera <jeanrivera@gmail.com>
2022-10-30 07:38:17 -0400

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_ASUS_AI2201.mk
+    $(LOCAL_DIR)/twrp_ASUS_AI2201.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_ASUS_AI2201-user \
-    omni_ASUS_AI2201-userdebug \
-    omni_ASUS_AI2201-eng
+    twrp_ASUS_AI2201-eng
